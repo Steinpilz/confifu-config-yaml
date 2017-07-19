56f066d0826c17eddce7847ece9e6e01a4d80e85
ivanbianko <ivan.bianko@stein-pilz.com>
2017-07-19 15:43:26 +0200

    switched build scrips to publish to nuget.org

--- scripts/build.fsx
+++ scripts/build.fsx
@@ -1,25 +1,20 @@
+// include Fake lib
 #r @"..\packages\FAKE\tools\FakeLib.dll"
-#r @"..\packages\Fria2.FakeBuild\tools\Steinpilz.DevFlow.Fake.Lib.dll"
-#r @"..\packages\Fria2.FakeBuild\tools\Fria2.FakeBuild.Lib.dll"
+#r @"..\packages\Steinpilz.DevFlow.Fake\tools\Steinpilz.DevFlow.Fake.Lib.dll"
+
 
 open Fake
-open Fria2.FakeBuild
-open Steinpilz.DevFlow.Fake
+open Steinpilz.DevFlow.Fake 
 
-let libParams = Fria2.FakeBuild.Lib.setup <| fun p -> 
+Lib.setup(fun p -> 
     { p with 
-        AppProjects = !!"src/app/**/*.*proj"
-        TestProjects = !!"src/test/**/*.*proj"
-        PublishProjects = !! "src/app/**/*.*proj"
-
+        PublishProjects = !!"src/app/**/*.csproj"
         UseDotNetCliToPack = true
-        // You can use NuGet command line tools as a fallback:
-        //UseNuGetToPack = true
-        //UseNuGetToRestore = true
-        //NuGetFeed = 
-        //    { p.NuGetFeed with 
-        //        ApiKey = environVarOrFail <| "NUGET_API_KEY" |> Some
-        //    }
+        NuGetFeed = 
+            { p.NuGetFeed with 
+                ApiKey = environVarOrFail <| "NUGET_API_KEY" |> Some
+            }
     }
+)
 
 RunTargetOrDefault "Watch"
